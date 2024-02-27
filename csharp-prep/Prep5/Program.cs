c4a1f0359c9f608f7d171e1228e7dae5ce44381d
ryder4676 <gottalovebasketball@hotmail.com>
2024-02-27 09:01:41 -0700

    prep 5 complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Call the DisplayMessage function
+        DisplayMessage();
+        // Call the DisplayPersonalMessage function and save the return value in a variable
+        string userName = DisplayPersonalMessage();
+        // Call the GetInteger function and save the return value in a variable
+        int userNumber = GetInteger();
+        // Call the SquaredNumber function with the user's number and save the result in a variable
+        int squaredNumber = SquaredNumber(userNumber);
+            // Call the DisplayNumberSquared function with the user's name and squared number
+        DisplayNumberSquared(userName, squaredNumber);
+
+
+    }
+    // Function to display a welcome message
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    // Function to prompt the user for their name and return it
+    static string DisplayPersonalMessage()
+    {
+        Console.Write("Please Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    // Function to prompt the user for their favorite number and return it
+    static int GetInteger()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        string numberInput = Console.ReadLine();
+        int userNumber = int.Parse(numberInput);
+        return userNumber;
+    }
+     // Function to square a given number and return the result
+    static int SquaredNumber(int userNumber)
+    {
+        return userNumber * userNumber;
+    }
+    // Function to display the user's name and squared number
+    static void DisplayNumberSquared(string userName, int SquaredNumber )
+    {
+        Console.Write($"{userName}, the square of your number is {SquaredNumber}");
     }
 }
