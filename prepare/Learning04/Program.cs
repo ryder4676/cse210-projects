2e9ac20c1ed083b9e56e1baf9d1bdaf9d8d3b5df
ryder4676 <gottalovebasketball@hotmail.com>
2024-03-19 13:39:38 -0600

    finished learning for inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("George Geezman", "How to program in C#");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Ninka Inka", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeWorkList());
+
+        WritingAssignment a3 = new WritingAssignment("Potter Stumpy", "Programming", "How to write in C#");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
